48396260a9bb7f45732f0f5f6266b4c141a8648d
Santrox <malongameplay123@gmail.com>
2025-12-06 13:38:35 -0500

    Actualizacion

---
+++ BaseDatos/mercapleno (1).sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-12-2025 a las 19:35:54
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mercapleno`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+-- Error leyendo la estructura de la tabla mercapleno.categoria: #1932 - Table &#039;mercapleno.categoria&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.categoria: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`categoria`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `devolver_productos`
+--
+-- Error leyendo la estructura de la tabla mercapleno.devolver_productos: #1932 - Table &#039;mercapleno.devolver_productos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.devolver_productos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`devolver_productos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrada_productos`
+--
+-- Error leyendo la estructura de la tabla mercapleno.entrada_productos: #1932 - Table &#039;mercapleno.entrada_productos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.entrada_productos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`entrada_productos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `metodo`
+--
+-- Error leyendo la estructura de la tabla mercapleno.metodo: #1932 - Table &#039;mercapleno.metodo&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.metodo: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`metodo`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimiento`
+--
+-- Error leyendo la estructura de la tabla mercapleno.movimiento: #1932 - Table &#039;mercapleno.movimiento&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.movimiento: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`movimiento`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+-- Error leyendo la estructura de la tabla mercapleno.productos: #1932 - Table &#039;mercapleno.productos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.productos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`productos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+-- Error leyendo la estructura de la tabla mercapleno.proveedor: #1932 - Table &#039;mercapleno.proveedor&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.proveedor: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`proveedor`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+-- Error leyendo la estructura de la tabla mercapleno.roles: #1932 - Table &#039;mercapleno.roles&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.roles: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`roles`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salida_productos`
+--
+-- Error leyendo la estructura de la tabla mercapleno.salida_productos: #1932 - Table &#039;mercapleno.salida_productos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.salida_productos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`salida_productos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `stock_actual`
+--
+-- Error leyendo la estructura de la tabla mercapleno.stock_actual: #1932 - Table &#039;mercapleno.stock_actual&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.stock_actual: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`stock_actual`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipos_identificacion`
+--
+-- Error leyendo la estructura de la tabla mercapleno.tipos_identificacion: #1932 - Table &#039;mercapleno.tipos_identificacion&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.tipos_identificacion: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`tipos_identificacion`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_devolucion`
+--
+-- Error leyendo la estructura de la tabla mercapleno.tipo_devolucion: #1932 - Table &#039;mercapleno.tipo_devolucion&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.tipo_devolucion: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`tipo_devolucion`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_movimiento`
+--
+-- Error leyendo la estructura de la tabla mercapleno.tipo_movimiento: #1932 - Table &#039;mercapleno.tipo_movimiento&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.tipo_movimiento: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`tipo_movimiento`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Error leyendo la estructura de la tabla mercapleno.usuarios: #1932 - Table &#039;mercapleno.usuarios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.usuarios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`usuarios`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+-- Error leyendo la estructura de la tabla mercapleno.venta: #1932 - Table &#039;mercapleno.venta&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.venta: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`venta`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta_productos`
+--
+-- Error leyendo la estructura de la tabla mercapleno.venta_productos: #1932 - Table &#039;mercapleno.venta_productos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla mercapleno.venta_productos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `mercapleno`.`venta_productos`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
